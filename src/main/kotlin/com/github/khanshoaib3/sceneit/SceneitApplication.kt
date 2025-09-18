50b8210ec38e4aaa65764e64a940767cbcf607da
Mohammad Shoaib Khan <shoaib.khan20@outlook.com>
2025-09-19 00:44:49 +0530

    Initial commit

---
+++ src/main/kotlin/com/github/khanshoaib3/sceneit/SceneitApplication.kt
@@ -0,0 +1,20 @@
+package com.github.khanshoaib3.sceneit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+class SceneitApplication {
+    @GetMapping("/hello")
+    fun hello(@RequestParam(value = "name", defaultValue = "World") name: String?): String {
+        return String.format("Hello %s!", name)
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<SceneitApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/khanshoaib3/sceneit/SceneitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.khanshoaib3.sceneit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SceneitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
